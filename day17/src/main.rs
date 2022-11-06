817a0e9dcebda4006d652f9013462e2596c55540
Josh Phillips <joshua.e.phillips@gmail.com>
2022-11-06 13:40:41 -0600

    Add code and tests for day17

---
+++ day17/src/main.rs
@@ -0,0 +1,106 @@
+use int_code_computer::Intcode;
+use std::fs;
+
+fn main() {
+    let contents = fs::read_to_string("input.txt").expect("File not found");
+    println!("{}", part1(contents.clone())); // 6672
+    println!("{}", part2(contents)); // 923017
+}
+
+fn part1(program: String) -> usize {
+    let mut comp = Intcode::intcode_instance(program);
+    comp.run();
+    let output = comp.get_outputs();
+    let o: String = output.iter().map(|i| (*i as u8) as char).collect();
+    let mut v: Vec<Vec<char>> = Vec::new();
+    for line in o.lines() {
+        let chars: Vec<char> = line.chars().collect();
+        v.push(chars);
+    }
+
+    let mut alignment = 0;
+    for y in 1..(v.len() - 2) {
+        let vec = v.get(y).unwrap();
+        for x in 1..(vec.len() - 1) {
+            if is_intersection(&mut v, y, x) {
+                alignment += x * y;
+            }
+        }
+    }
+    print_map(&v);
+    alignment
+}
+
+fn part2(program: String) -> i64 {
+    let mut comp = Intcode::intcode_instance(program);
+    comp.write(2, 0);
+    comp.run();
+    for b in "B,C,C,A,A,B,B,C,C,A\n".as_bytes() {
+        // Main program
+        comp.set_input(*b as i64);
+        comp.run();
+    }
+    for b in "R,12,R,4,L,6,L,8,L,8\n".as_bytes() {
+        // Program A
+        comp.set_input(*b as i64);
+        comp.run();
+    }
+    for b in "L,12,R,4,R,4\n".as_bytes() {
+        // Program B
+        comp.set_input(*b as i64);
+        comp.run();
+    }
+    for b in "R,12,R,4,L,12\n".as_bytes() {
+        // Program C
+        comp.set_input(*b as i64);
+        comp.run();
+    }
+    for b in "n\n".as_bytes() {
+        // No video feed
+        comp.set_input(*b as i64);
+        comp.run();
+    }
+    comp.get_last_output()
+}
+
+fn is_intersection(map: &mut [Vec<char>], row: usize, col: usize) -> bool {
+    let c = map[row][col];
+    if c != '#' {
+        return false;
+    }
+    let neighbors = vec![(0, 1), (0, -1), (1, 0), (-1, 0)];
+    for neighbor in neighbors {
+        let r = row as i32 + neighbor.0;
+        let c = col as i32 + neighbor.1;
+        if map[r as usize][c as usize] != '#' {
+            return false;
+        }
+    }
+    true
+}
+
+fn print_map(map: &[Vec<char>]) {
+    for row in map {
+        for c in row {
+            print!("{c}");
+        }
+        println!();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        let contents = fs::read_to_string("input.txt").expect("File not found");
+        assert_eq!(part1(contents), 6672);
+    }
+
+    #[test]
+    fn test_part2() {
+        let contents = fs::read_to_string("input.txt").expect("File not found");
+        assert_eq!(part2(contents), 923017);
+    }
+}
